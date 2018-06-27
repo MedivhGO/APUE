58ac7f1b4e939be2b5cde9302fad682970a58605
MedivhGO <lijingqi93@gmail.com>
2018-06-27 21:23:47 +0800

    APUE Clion

---
+++ cmake-build-debug/CMakeFiles/APUE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APUE.dir/main.cpp.o"
+  "APUE.pdb"
+  "APUE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APUE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/APUE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for APUE.
+# This may be replaced when dependencies are built.
