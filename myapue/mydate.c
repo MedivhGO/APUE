6fd24f66acfeb8de13f9fdb4ad6229b12f52fdd9
MedivhGO <lijingqi93@gmail.com>
2020-10-07 16:56:06 +0800

    add lesson42 code

--- myapue/mydate.c
+++ myapue/mydate.c
@@ -17,65 +17,58 @@
 
 #define TIMESTARSIZE 1024
 #define FMTSTRSIZE 1024
-int main()
+int main(int argc,char** argv)
 {
 	time_t stamp;
 	struct tm *tm;
 	char timestr[TIMESTARSIZE];
 	int c;
-	
+
 	char fmtstr[FMTSTRSIZE];
-	
+
 	fmtstr[0] = '\0';
-	
+
 	stamp = time(NULL);
 	tm = localtime(&stamp);
-	
-	while(1) 
+
+	while(1)
 	{
-		
+
 		c =  getopt(argc,argv,"HMSymd");
-		if (c < 0) 
+		if (c < 0)
 			break;
-		
+
 		switch(c)
 		{
 			case 'H' :
-			
+				strncat(fmtstr,"%H",FMTSTRSIZE);
 				break;
 			case 'M' :
-				
-				strcat(fmtstr,"%M",FMTSTRSIZE);
-			
+				strncat(fmtstr,"%M",FMTSTRSIZE);
 				break;
-			
 			case 'S' :
-			
+				strncat(fmtstr,"%S",FMTSTRSIZE);
 				break;
-			
 			case 'y' :
-			
+				strncat(fmtstr,"%y",FMTSTRSIZE);
 				break;
-			
 			case 'm' :
-			
+				strncat(fmtstr,"%m",FMTSTRSIZE);
 				break;
-			
 			case 'd' :
-			
+				strncat(fmtstr,"%d",FMTSTRSIZE);
 				break;
-			
 			default:
 				break;
-			
+
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 	//strftime(timestr,TIMESTARSIZE,"Now: %Y-%m-%d",tm);
-	strftime(timestr,TIMESTARSIZE,fmtstr,tm);
+	strftime(timestr,TIMESTARSIZE,fmtstr,tm); //使用给定格式输出字符串
 	puts(timestr);
 
 	//tm->tm_mday += 100;
