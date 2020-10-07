927a7cc918cb4acae25690348d6a5e7c96553bb0
MedivhGO <lijingqi93@gmail.com>
2020-10-07 18:18:35 +0800

    add lesson42 code

--- myapue/commandlineparam.md
+++ myapue/commandlineparam.md
@@ -1,3 +1,6 @@
 3 命令行参数分析
 	getopt();
 	getopt_long(); //分析长格式 例如--all
+
+
+    extern char *optarg; //指向有 “：”修饰的 选项字符串
--- myapue/mydate.c
+++ myapue/mydate.c
@@ -23,6 +23,7 @@ int main(int argc,char** argv)
 	struct tm *tm;
 	char timestr[TIMESTARSIZE];
 	int c;
+	FILE* fp = stdout;
 
 	char fmtstr[FMTSTRSIZE];
 
@@ -33,43 +34,63 @@ int main(int argc,char** argv)
 
 	while(1)
 	{
-
-		c =  getopt(argc,argv,"HMSymd");
+		c =  getopt(argc,argv,"-H:MSy:md"); //-开头，遇到非选项的传参时，返回值为1
 		if (c < 0)
 			break;
 
 		switch(c)
 		{
+			case '1' :
+				if (fp == stdout) {
+						fp = fopen(argv[optind-1],"w"); //optind 当前的argv下标
+					if (fp == NULL) {
+						perror("fopen()");
+						fp = stdout;
+					}
+				}
+				break;
 			case 'H' :
-				strncat(fmtstr,"%H",FMTSTRSIZE);
+				if (strcmp(optarg,"12")==0) {
+					strncat(fmtstr,"%I(%P) ",FMTSTRSIZE);
+				} else if (strcmp(optarg,"24") == 0) {
+					strncat(fmtstr,"%H ",FMTSTRSIZE);
+				} else {
+					fprintf(stderr,"Invalid argument");	
+				}
 				break;
 			case 'M' :
-				strncat(fmtstr,"%M",FMTSTRSIZE);
+				strncat(fmtstr,"%M ",FMTSTRSIZE);
 				break;
 			case 'S' :
-				strncat(fmtstr,"%S",FMTSTRSIZE);
+				strncat(fmtstr,"%S ",FMTSTRSIZE);
 				break;
 			case 'y' :
-				strncat(fmtstr,"%y",FMTSTRSIZE);
+				if (strcmp(optarg,"2")==0) {
+					strncat(fmtstr,"%y",FMTSTRSIZE);
+				} else if (strcmp(optarg,"4")==0) {
+					strncat(fmtstr,"%Y",FMTSTRSIZE);
+				} else {
+					fprintf(stderr,"Invalid argument");	
+				}
 				break;
 			case 'm' :
-				strncat(fmtstr,"%m",FMTSTRSIZE);
+				strncat(fmtstr,"%m ",FMTSTRSIZE);
 				break;
 			case 'd' :
-				strncat(fmtstr,"%d",FMTSTRSIZE);
+				strncat(fmtstr,"%d ",FMTSTRSIZE);
 				break;
 			default:
 				break;
-
 		}
-
-
-
 	}
 
 	//strftime(timestr,TIMESTARSIZE,"Now: %Y-%m-%d",tm);
+	strncat(fmtstr,"\n",FMTSTRSIZE);
 	strftime(timestr,TIMESTARSIZE,fmtstr,tm); //使用给定格式输出字符串
-	puts(timestr);
+	fputs(timestr,fp);
+	
+	if (fp != stdout) 
+		fclose(fp);
 
 	//tm->tm_mday += 100;
 	//(void)mktime(tm);
