777d3480c0eeabb8d73e2a81c6586a6865d240a6
MedivhGO <lijingqi93@gmail.com>
2020-10-13 20:01:03 +0800

    add lesson56 code

--- myapue/process_basic/myshell.c
+++ myapue/process_basic/myshell.c
@@ -1,54 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <glob.h>
 
+#define DELIMS " \t\n"
 
+struct cmd_st
+{
+    glob_t globres;
+};
 
+static void prompt(void)
+{
+    printf("mysh-0.1$");
+}
+static void parse(char *line, struct cmd_st *res)
+{
+    char *tok;
+    int i = 0;
+
+    while (1)
+    {
+        tok = strsep(&line, DELIMS);
+        if (tok == NULL)
+            break;
+        if (tok[0] == '\0')
+            continue;
+        glob(tok, GLOB_NOCHECK | GLOB_APPEND * i, NULL, &res->globres);
+        i = 1;
+    }
+}
 int main()
 {
 
+    while (1)
+    {
 
-    while (1) {
+        char *linebuf = NULL;
+        size_t linebuf_size = 0;
+        struct cmd_st cmd;
+        pid_t pid;
 
         prompt();
 
-        getline();
+        if (getline(&linebuf, &linebuf_size, stdin) < 0)
+            break;
 
-        parse();
+        parse(linebuf, &cmd);
 
-        if (internal_commond){
+        if (0)
+        {
 
-            do_some();
+            //do_something
+        }
+        else
+        {
 
-        } else {
+            pid = fork();
 
-            fork();
+            if (pid < 0)
+            {
 
-            if (<0) {
-
-
-            }
-
-            if (==0) {
-                execle()
-                perror();
+                perror("fork");
                 exit(1);
             }
 
-
-
-            wait();
-
-
-
-
+            if (pid == 0)
+            {
+                execvp(cmd.globres.gl_pathv[0],cmd.globres.gl_pathv);
+                perror("execvp");
+                exit(1);
+            }
+            wait(NULL);
         }
-
-
-
     }
-
-
-
-
 }
