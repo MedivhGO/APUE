0da3faa2e38be1c33e431c186f874d1bf06aba9a
MedivhGO <lijingqi93@gmail.com>
2020-10-14 20:50:35 +0800

    add lesson61 code

---
+++ myapue/process_basic/daemon/mydaemon.c
@@ -0,0 +1,49 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+
+#define FNAME "/tmp/out"
+
+
+static void daemonize()
+{
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork error");
+        exit(1);
+    }
+
+    if (pid > 0) {
+        exit(0);
+    }
+
+    setsid();
+
+}
+
+int main()
+{
+
+    FILE* fp;
+    int i;
+    daemonize();
+
+    fp = fopen(FNAME,"w");
+
+    if (fp == NULL) {
+        perror("fopen()");
+        exit(1);
+    }
+
+    for (i = 0;;i++) {
+        fprintf(fp,"%d\n",i);
+        fflush(fp);
+        sleep(1);
+    }
+
+    exit(1);
+}
